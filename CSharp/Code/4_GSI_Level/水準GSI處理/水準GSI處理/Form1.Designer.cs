713b027a7fcb6280fec7049ed5d90dc575264e3d
innowantai <innowantai@gmail.com>
2020-07-30 22:39:29 +0800

    20200730 GSI doing

---
+++ CSharp/Code/4_GSI_Level/水準GSI處理/水準GSI處理/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace 水準GSI處理
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.btn_執行 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_執行
+            // 
+            this.btn_執行.Location = new System.Drawing.Point(360, 237);
+            this.btn_執行.Name = "btn_執行";
+            this.btn_執行.Size = new System.Drawing.Size(110, 48);
+            this.btn_執行.TabIndex = 0;
+            this.btn_執行.Text = "執行";
+            this.btn_執行.UseVisualStyleBackColor = true;
+            this.btn_執行.Click += new System.EventHandler(this.btn_執行_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_執行);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button btn_執行;
+    }
+}
+
---
+++ CSharp/Code/4_GSI_Level/水準GSI處理/水準GSI處理/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 水準GSI處理
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_執行_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\innow\Desktop\MeasureDataProcessingUs\CSharp\Code\4_GSI_Level\TryData\M_0616JD\M_0616JD.GSI";
+
+            this.LoadingGSIData(filePath);
+        }
+
+
+
+        private void LoadingGSIData(string filePath)
+        {
+            List<string> tmpData = new List<string>();
+            using ( StreamReader sr = new StreamReader(filePath))
+            {
+                while (sr.Peek() != -1)
+                {
+                    tmpData.Add(sr.ReadLine());
+                }
+            }
+
+            List<string> tmp = new List<string>();
+            List<List<string>> GroupDatas = new List<List<string>>();
+            for (int i = 0; i < tmpData.Count; i++)
+            {
+                if (tmpData[i].Length == 25)
+                {
+                    if (tmp.Count != 0) GroupDatas.Add(tmp); 
+                    tmp = new List<string>();
+                }
+                else if (tmpData[i].Length == 145)
+                {
+                    tmp.Add(tmpData[i - 4]);
+                    tmp.Add(tmpData[i - 3]);
+                    tmp.Add(tmpData[i - 2]);
+                    tmp.Add(tmpData[i - 1]);
+                }
+            }
+            GroupDatas.Add(tmp);
+            /// 25    145
+            /// 
+
+        }
+    }
+}
